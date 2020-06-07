279cccaaa11640a1a71abb7e46d24a24bf9c9312
AhmedIbrahimHamed <aih1175@gmail.com>
2020-06-07 13:40:32 +0200

    Updates

---
+++ precache-manifest.2e2be0650c015c640068fde57211a0d1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5127e47e25b2aa04f125607f3e1d9043",
+    "url": "/Ristorante-Con-Fusion/index.html"
+  },
+  {
+    "revision": "5be39b78af1e6d8a6c40",
+    "url": "/Ristorante-Con-Fusion/static/css/2.b9b88926.chunk.css"
+  },
+  {
+    "revision": "1b75a290aa5aa1dddc4f",
+    "url": "/Ristorante-Con-Fusion/static/css/main.b9ff4e28.chunk.css"
+  },
+  {
+    "revision": "5be39b78af1e6d8a6c40",
+    "url": "/Ristorante-Con-Fusion/static/js/2.ae5538d2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Ristorante-Con-Fusion/static/js/2.ae5538d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b75a290aa5aa1dddc4f",
+    "url": "/Ristorante-Con-Fusion/static/js/main.5fa67781.chunk.js"
+  },
+  {
+    "revision": "527c7a7c75b66eb14e2b",
+    "url": "/Ristorante-Con-Fusion/static/js/runtime-main.7bda75fd.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Ristorante-Con-Fusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Ristorante-Con-Fusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Ristorante-Con-Fusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Ristorante-Con-Fusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Ristorante-Con-Fusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
