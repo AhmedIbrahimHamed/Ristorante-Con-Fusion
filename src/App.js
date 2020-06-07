5df849b1ca4f6a7f3a0070043923a1398459b937
AhmedIbrahimHamed <aih1175@gmail.com>
2020-06-07 14:16:15 +0200

    fix navigation

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router basename="/">
+        <Router basename="/Ristorante-Con-Fusion">
           <div className="App">
             <Main />
           </div>
