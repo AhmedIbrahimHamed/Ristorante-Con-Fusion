58fb16f6e9f65eb813bc670dfb03a777e87299c1
AhmedIbrahimHamed <aih1175@gmail.com>
2020-06-07 14:11:13 +0200

    Fix navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Main from './components/MainComponent';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 
@@ -12,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router basename="/Ristorante-Con-Fusion">
+        <Router basename="/">
           <div className="App">
             <Main />
           </div>
